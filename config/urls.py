97dbd6fc69e7a6525117df3cde4030e10ae55b29
Pablo Tisalema <pablo.tisalema9488@utc.edu.ec>
2026-08-05 22:58:07 -0500

    fix: rutas PWA configuradas para sw.js y manifest

--- config/urls.py
+++ config/urls.py
@@ -1,30 +1,26 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-# config/urls.py
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView, RedirectView # <-- Importamos RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('estudio.urls')),  # <-- Esto incluye automáticamente todas las rutas de libros/urls.py
+
+    # Service Worker desde la plantilla
+    path('sw.js', TemplateView.as_view(
+        template_name='estudio/sw.js', 
+        content_type='application/javascript'
+    ), name='sw.js'),
+    
+    # Manifest redirigido al archivo estático
+    path('manifest.json', RedirectView.as_view(
+        url='/static/manifest.json', 
+        permanent=True
+    ), name='manifest.json'),
+
+    path('', include('estudio.urls')),
 ]
 
-# Esto permite a Django servir tus archivos de portada, PDFs y comprobantes de pago localmente
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
